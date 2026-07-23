41af5999b88f5fc066eb796f8854aef1d1584a68
guyguy234 <guyglam2@gmail.com>
2026-07-23 16:33:27 +0300

    Update chat.js

--- T-embed/chat.js
+++ T-embed/chat.js
@@ -1,8 +1,9 @@
 var wifi = require("wifi");
 var display = require("display");
 var keyboard = require("keyboard");
+//change to your server IP
+var SERVER_URL = "YOUR_SERVER_IP:13959/chat";
 
-var SERVER_URL = "http://78.154.103.27:13959/chat";
 
 function printWrappedText(text, maxCharsPerLine) {
   var words = text.split(" ");
@@ -30,9 +31,11 @@ function showResponse(text) {
   display.fill(black);
   display.setTextSize(1);
 
+
   display.setTextColor(cyan);
   display.println("--- AI Response ---");
   display.println("");
+
 
   display.setTextColor(white);
   printWrappedText(text, 24);
@@ -44,11 +47,13 @@ function showResponse(text) {
 
 function startChat() {
   while (true) {
+
     var userInput = keyboard.keyboard("", 100, "Prompt AI:");
 
     if (!userInput || userInput.length === 0) {
       break;
     }
+
 
     display.fill(display.color(0, 0, 0));
     display.setTextColor(display.color(255, 255, 0));
