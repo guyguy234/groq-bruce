acccdefa483ae89b1af53a964c19b595bb5c8308
guyguy234 <guyglam2@gmail.com>
2026-07-23 18:37:41 +0300

    Update chat.js

--- T-embed/chat.js
+++ T-embed/chat.js
@@ -1,9 +1,10 @@
+// Bruce OS - Gemini/Groq Chat (chat.js)
 var wifi = require("wifi");
 var display = require("display");
 var keyboard = require("keyboard");
-//change to your server IP
-var SERVER_URL = "YOUR_SERVER_IP:13959/chat";
 
+//change to your server IP
+var SERVER_URL = "http://YOUR_SERVER_IP:13959/chat";
 
 function printWrappedText(text, maxCharsPerLine) {
   var words = text.split(" ");
@@ -31,11 +32,10 @@ function showResponse(text) {
   display.fill(black);
   display.setTextSize(1);
 
-
+   
   display.setTextColor(cyan);
   display.println("--- AI Response ---");
   display.println("");
-
 
   display.setTextColor(white);
   printWrappedText(text, 24);
@@ -53,7 +53,6 @@ function startChat() {
     if (!userInput || userInput.length === 0) {
       break;
     }
-
 
     display.fill(display.color(0, 0, 0));
     display.setTextColor(display.color(255, 255, 0));
